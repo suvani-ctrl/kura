ac57143567f04b1bc2681b0ab79de01e929dcb11
suvani-ctrl <suvanibasnet8@gmail.com>
2026-03-08 22:43:45 +0545

    Remove null image and file fields from messages collection

--- backend/migrations/migrate.js
+++ backend/migrations/migrate.js
@@ -2,7 +2,7 @@ import {connectDB} from "../src/lib/db.js"
 import {removeNull} from "./20260308093922_remove_null_fields.js"
 import mongoose from "mongoose";
 
-export const migration = async() =>{
+// export const migration = async() =>{
     try{
         await connectDB();
         await removeNull(mongoose.connection.db);
