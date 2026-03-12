46955b91fb4f2eec1fd14f2fdd0fb5dcd2ae52fb
suvani-ctrl <suvanibasnet8@gmail.com>
2026-03-13 01:24:03 +0545

    Refractored

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,8 @@ const corsOptions = {
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 };
+
+
 app.use(morgan("dev"))
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
---
+++ backend/socket/socketServer.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import {ENV} from "../src/lib/env.js"
+import http,{Server} from "http"
+import {Server as SocketIOServer} from 'socket.io'
+import cors from 'cors'
+
+const allowedOrigin = [
+"http://localhost:5173",
+ENV.CLIENT_URL
+].filter(Boolean);
+
+export const initializeSocketServer(server){
+  const io = new SocketIOServer(server,{
+    cors:{
+      origin:(origin,callback) =>{
+        if(!origin || allowedOrigin.includes(origin)){
+          callback(null,true)
+        }else{
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      method: ['GET','POST'],
+      credentials: true
+    }
+  })
+}
+
+const app = express();
